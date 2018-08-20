41e04fa05b107f5a2d96b66b79e0a527bef94510
Tania Rascia <taniarascia@gmail.com>
2018-08-19 22:52:40 -0500

    Update package.json

--- src/App.js
+++ src/App.js
@@ -26,10 +26,13 @@ class App extends Component {
         
         return (
             <div className="container">
+                <h1>React Tutorial</h1>
+                <p>Add a character with a name and a job to the table.</p>
                 <Table
                     characterData={characters}
                     removeCharacter={this.removeCharacter}
                 />
+                <h3>Add New</h3>
                 <Form handleSubmit={this.handleSubmit} />
             </div>
         );
