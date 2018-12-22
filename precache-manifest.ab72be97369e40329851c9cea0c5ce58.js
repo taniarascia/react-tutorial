6f75207cce934ddd6eab6856a98e92c04a737ce8
Tania Rascia <taniarascia@gmail.com>
2018-12-22 16:18:11 -0600

    Updates

---
+++ precache-manifest.ab72be97369e40329851c9cea0c5ce58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "0caa5015f24ba7f4fbdb",
+    "url": "/react-tutorial/static/js/main.0caa5015.chunk.js"
+  },
+  {
+    "revision": "f3b84ad1a4a60d245526",
+    "url": "/react-tutorial/static/js/1.f3b84ad1.chunk.js"
+  },
+  {
+    "revision": "0caa5015f24ba7f4fbdb",
+    "url": "/react-tutorial/static/css/main.40b27602.chunk.css"
+  },
+  {
+    "revision": "795343830218917f16b0540da44489c4",
+    "url": "/react-tutorial/index.html"
+  }
+];
