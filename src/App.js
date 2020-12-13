d3aab0e612350bc66c2613f56077bbdb25537c4a
junlugrace <junlu05@gmail.com>
2020-12-13 14:39:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import React, { Component } from 'react';
-import Table from './Table';
-import Form from './Form';
+import React, {Component} from 'react'
+import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
-    state = {
-        characters: []
-    };
-
-    removeCharacter = index => {
-        const { characters } = this.state;
+  state = {
+   messages: []
+  };
+  
+  removeMessage = index => {
+    const { messages } = this.state;
     
-        this.setState({
-            characters: characters.filter((character, i) => { 
-                return i !== index;
-            })
-        });
-    }
+    this.setState({
+      messages: messages.filter((message, i) => { 
+        return i !== index;
+      })
+    });
+  }
+  
+  handleSubmit = (message) => {
+	this.setState({messages: [...this.state.messages, message]})
+  }
+  
+  render() {
+    const { messages } = this.state;
 
-    handleSubmit = character => {
-        this.setState({characters: [...this.state.characters, character]});
-    }
-
-    render() {
-        const { characters } = this.state;
-        
-        return (
-            <div className="container">
-                <h1>React Tutorial</h1>
-                <p>Add a character with a name and a job to the table.</p>
-                <Table
-                    characterData={characters}
-                    removeCharacter={this.removeCharacter}
-                />
-                <h3>Add New</h3>
-                <Form handleSubmit={this.handleSubmit} />
-            </div>
-        );
-    }
+	return (
+	  <div className="container">
+        <h1>Chatting</h1>
+        <Table
+          messagesData={messages}
+          removeMessage={this.removeMessage}
+        />
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+	)
+  }
 }
-
-export default App;
+export default App
